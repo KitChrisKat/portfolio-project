0e4f9337fb024064e020a824ebbca369663d8a45
Chris Pangilinan <kit@Chriss-MacBook-Air.local>
2025-08-01 18:51:30 +0800

    Initial portfolio commit

---
+++ typing-effect.js
@@ -0,0 +1,22 @@
+document.addEventListener('DOMContentLoaded', function () {
+  const first = new Typed('#line1', {
+    strings: ["Hi! I'm Chris"],
+    typeSpeed: 70,
+    showCursor: true,
+    cursorChar: '|',
+    onComplete(self) {
+      // correctly remove the cursor from the first instance
+      self.cursor.remove();
+
+      // optional delay before second line
+      setTimeout(() => {
+        new Typed('#line2', {
+          strings: ['Pangilinan'],
+          typeSpeed: 70,
+          showCursor: true,
+          cursorChar: '|',
+        });
+      }, 300);
+    }
+  });
+});
